4c63e8d9e32baba3925fe86dc598e8f55d687f36
Akash <akashsardar383@gmail.com>
2022-03-22 03:18:50 +0530

    Final Commit

--- recommender/views.py
+++ recommender/views.py
@@ -46,29 +46,41 @@ def recommend_movie(movie_name):
     score_list=sorted(enum_list,reverse=True,key=lambda x:x[1])
     score_list=score_list[1:11]
 
+    rec_mov_id=[]
     rec_mov_name=[]
     rec_mov_poster=[]
     for i in score_list:
+        rec_mov_id.append((movies['id'].iloc[i[0]]))
         rec_mov_name.append((movies['title'].iloc[i[0]]))
         rec_mov_poster.append(fetch_poster(movies['id'].iloc[i[0]]))
         print(movies['title'].iloc[i[0]]) #movie names
-    return rec_mov_name,rec_mov_poster
+    return rec_mov_id,rec_mov_name,rec_mov_poster
     
 def firstpage(request):
     from register import views as v
     return v.register(request)
 
-def home(request,user):
-    return render(request,'Homepage.html',{'user':user})
+def home(request):
+    return render(request,'Homepage.html')
 
 
 def output(request):
     movie_name=str(request.POST.get('movie_name'))
     try:
-        name,poster=recommend_movie(movie_name)
-        My_dict={str(i+1)+". "+name[i]:poster[i] for i in range(len(name))}
+        ids,name,poster=recommend_movie(movie_name)
+        key_list=[]
+        for id in ids:
+            url= f"https://api.themoviedb.org/3/movie/{id}/videos?api_key=06412c2ac60d3b3a66c7fb129dcaca28&append_to_response=videost"
+            response=requests.get(url)
+            information=response.json()
+            key_list.append(information['results'][0]['key'])
+        print(key_list)
+
+        My_dict={str(i+1)+". "+name[i]:[poster[i],key_list[i]] for i in range(len(name))}
         # print(My_dict.keys(),My_dict.values())
+        print("okkkkkkkkkkkkkkkk")
         return render(request,'Homepage.html',{'result':My_dict, 'movie_name':movie_name, 'status':True})    
+    
     except:
         return render(request,'Warning.html',{'movie_name':movie_name})
 
